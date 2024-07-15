15a145884fb0253fb55fd468dadc2f6442d19f8e
Kathryn Chu <34584512+saskatchuwan@users.noreply.github.com>
2024-07-15 15:13:32 -0700

    Fix issues with file processing when creating zip source file

--- modules/data_pipeline/main.tf
+++ modules/data_pipeline/main.tf
@@ -2,42 +2,48 @@ data "google_project" "project" {
 }
 
 locals {
-  source_files = concat(
-    # Add paths to files from /cloud_functions's root
-    [
+  root_source_files =  [
       "${path.module}/../../climateiq-cnn/usl_pipeline/cloud_functions/main.py",
       "${path.module}/../../climateiq-cnn/usl_pipeline/cloud_functions/requirements.txt"
-    ],
-    # Add paths to all .py files from /usl_lib subdirectory
-    tolist(fileset("${path.module}/../../climateiq-cnn/usl_pipeline/usl_lib/usl_lib/", "**/*.py")),
-    # Add paths to all .whl files from /cloud_functions's /wheels subdirectory
-    tolist(fileset("${path.module}/../../climateiq-cnn/usl_pipeline/cloud_functions/wheels/", "*.whl"))
-  )
+  ]
+  wheels_source_files = tolist(fileset("{path.module}/../../climateiq-cnn/usl_pipeline/cloud_functions/wheels/", "*.whl"))
+  usl_lib_source_files = tolist(fileset("${path.module}/../../climateiq-cnn/usl_pipeline/usl_lib/usl_lib/", "**/*.py"))
 
-  usl_lib_dir = "${path.module}/../../climateiq-cnn/usl_pipeline/usl_lib/usl_lib/"
   wheels_dir = "${path.module}/../../climateiq-cnn/usl_pipeline/cloud_functions/wheels/"
+  usl_lib_dir = "${path.module}/../../climateiq-cnn/usl_pipeline/usl_lib/usl_lib/"
 }
 
-# Read and process the content from source_files
+# Process and copy files root cloud function files to temp directory.
+# A `template_file` data block is used here to first process each files content
+# and make it available to use elsewhere in this file. This is necessary for files
+# defined in `locals` block that is not dynamically discovered (i.e. fileset()).
 data "template_file" "t_file" {
-  count = "${length(local.source_files)}"
-  template = element(local.source_files, count.index)
+  count = "${length(local.root_source_files)}"
+  template = "${element(local.root_source_files, count.index)}"
 }
 
-# Copy files to a temporary directory to prep for zip
-resource "local_file" "to_temp_dir" {
-  count    = "${length(local.source_files)}"
+resource "local_file" "to_temp_dir_root" {
+  count = "${length(local.root_source_files)}"
 
-  # Filenames are constructed to preserve source subdirectory structure within /temp
-  filename = "${path.module}/temp/${
-    contains(fileset(local.usl_lib_dir, "**/"), element(local.source_files, count.index)) ?        
-      "usl_lib/${element(local.source_files, count.index)}" : 
-    contains(fileset(local.wheels_dir, "*"), element(local.source_files, count.index)) ? 
-      "wheels/${element(local.source_files, count.index)}" :
-    basename(element(local.source_files, count.index))
-  }"
+  filename = "${path.module}/temp/${basename(element(local.root_source_files, count.index))}"
+  content  = file(element(data.template_file.t_file.*.rendered, count.index))
+}
 
-  content  = "${element(data.template_file.t_file.*.rendered, count.index)}"
+# Copy /wheels files to temp directoryg
+resource "local_file" "to_temp_dir_wheels" {
+  count = "${length(local.wheels_source_files)}"
+
+  filename = "${path.module}/temp/wheels/${basename(element(local.wheels_source_files, count.index))}"
+  # Use base64 since wheel files are binary
+  content_base64 = filebase64("${local.wheels_dir}${element(local.wheels_source_files, count.index)}")
+}
+
+# Process /usl_lib files
+resource "local_file" "to_temp_dir_usl_lib" {
+  count = length(local.usl_lib_source_files)
+
+  filename = "${path.module}/temp/usl_lib/${element(local.usl_lib_source_files, count.index)}"
+  content  = file("${local.usl_lib_dir}${element(local.usl_lib_source_files, count.index)}")
 }
 
 # Place the source code for the cloud function into a GCS bucket.
@@ -47,7 +53,9 @@ data "archive_file" "source" {
   source_dir  = "${path.module}/temp"
 
   depends_on = [
-    local_file.to_temp_dir,
+    local_file.to_temp_dir_root,
+    local_file.to_temp_dir_wheels,
+    local_file.to_temp_dir_usl_lib,
   ]
 }
 

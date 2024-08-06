bd6b85444b23f02329cb73bffed767b236fcb5c8
Sunny Mak <smak@google.com>
2024-08-06 22:49:40 +0000

    Update configs for export pipeline.
    * merge and trigger_export are now Cloud Run Jobs triggered by Workflows.
    * trigger_export writes to a Cloud Tasks queue instead of PubSub.
    * Remove unnecessary depends_on.

--- modules/export_pipeline/merge_scenario_predictions.tf
+++ modules/export_pipeline/merge_scenario_predictions.tf
@@ -1,41 +1,106 @@
-# Create bucket for spatialized merged predictions output.
-resource "google_storage_bucket" "spatialized_merged_predictions" {
-  name     = "${var.bucket_prefix}climateiq-spatialized-merged-predictions"
-  location = var.bucket_region
-}
-
 # Create a service account used by the function and Eventarc trigger
 resource "google_service_account" "merge_scenario_predictions" {
   account_id   = "gcf-merge-predictions-sa"
   display_name = "Used By Merge Scenario Predictions Cloud Function"
 }
 
-# Grant permissions needed for the cloud function to run and receive event triggers.
-resource "google_project_iam_member" "merge_scenario_predictions_invoking" {
-  project    = data.google_project.project.project_id
-  role       = "roles/run.invoker"
-  member     = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
-  depends_on = [google_project_iam_member.gcs_pubsub_publishing]
+# Create bucket for spatialized merged predictions output.
+resource "google_storage_bucket" "spatialized_merged_predictions" {
+  name     = "${var.bucket_prefix}climateiq-spatialized-merged-predictions"
+  location = var.bucket_region
 }
 
+# Grant permission to invoke workflows
+resource "google_project_iam_member" "merge_scenario_predictions_workflows_invoker" {
+  project = data.google_project.project.id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
+}
+
+# Create a Cloud Workflow which watches the input bucket and triggers tasks.
+resource "google_workflows_workflow" "merge_scenario_predictions" {
+  name            = "merge-scenario-predictions"
+  region          = var.bucket_region
+  description     = "Watches climateiq-spatialized-chunk-predictions bucket for new files and triggers trigger-export-pipeline CF"
+  service_account = google_service_account.merge_scenario_predictions.id
+  source_contents = <<-EOF
+  main:
+      params: [event]
+      steps:
+          - init:
+              assign:
+                  - project_id: $${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")}
+                  - event_bucket: $${event.data.bucket}
+                  - event_file: $${event.data.name}
+                  - job_name: merge-scenario-predictions
+                  - job_location: ${var.bucket_region}
+          - run_job:
+              call: googleapis.run.v1.namespaces.jobs.run
+              args:
+                  name: $${"namespaces/" + project_id + "/jobs/" + job_name}
+                  location: $${job_location}
+                  body:
+                      overrides:
+                          containerOverrides:
+                              args: ["python3", "main.py", '$${event_file}']
+              result: job_execution
+          - finish:
+              return: $${job_execution}
+EOF
+
+  depends_on = [google_project_service.workflows]
+}
+
+# Grant permission to receive events
 resource "google_project_iam_member" "merge_scenario_predictions_receiving" {
-  project    = data.google_project.project.project_id
+  project    = data.google_project.project.id
   role       = "roles/eventarc.eventReceiver"
   member     = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
-  depends_on = [google_project_iam_member.merge_scenario_predictions_invoking]
+  depends_on = [google_project_service.eventarc]
+}
+
+# Create an Eventarc trigger, routing Cloud Storage events to Workflows
+resource "google_eventarc_trigger" "spatialized_chunk_created" {
+  name     = "spatialized-chunk-created"
+  location = var.bucket_region
+
+  # Capture objects changed in the bucket
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.spatialized_chunk_predictions.name
+  }
+
+  # Send events to Workflow
+  destination {
+    workflow = google_workflows_workflow.merge_scenario_predictions.id
+  }
+
+  service_account = google_service_account.merge_scenario_predictions.id
+
+  depends_on = [google_project_service.eventarc]
+}
+
+# Grant permissions needed for the Cloud Run Job to run.
+resource "google_project_iam_member" "merge_scenario_predictions_invoking" {
+  project    = data.google_project.project.project_id
+  role       = "roles/run.developer"
+  member     = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
 }
 
 resource "google_project_iam_member" "merge_scenario_predictions_artifactregistry_reader" {
   project    = data.google_project.project.project_id
   role       = "roles/artifactregistry.reader"
   member     = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
-  depends_on = [google_project_iam_member.merge_scenario_predictions_receiving]
 }
 
-# Give read access to Firestore.
-resource "google_project_iam_member" "merge_scenario_predictions_firestore_reader" {
+# Give read/write access to Firestore.
+resource "google_project_iam_member" "merge_scenario_predictions_firestore_user" {
   project = data.google_project.project.project_id
-  role    = "roles/datastore.viewer"
+  role    = "roles/datastore.user"
   member = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
 }
 
@@ -53,72 +118,33 @@ resource "google_storage_bucket_iam_member" "merge_scenario_predictions_spatiali
   member = "serviceAccount:${google_service_account.merge_scenario_predictions.email}"
 }
 
-# Place the source code for the cloud function into a GCS bucket.
-data "archive_file" "merge_scenario_predictions_source" {
-  type        = "zip"
-  output_path = "${path.module}/files/merge_scenario_predictions/cloud_function_source.zip"
+# Create Cloud Run Job.
+resource "google_cloud_run_v2_job" "merge_scenario_predictions" {
+  name         = "merge-scenario-predictions"
+  location     = var.bucket_region
+  project      = data.google_project.project.project_id
 
-  # Add main.py to the root of the zip file.
-  source {
-    content  = file("{path.module}/../../climateiq-frontend/cloud_functions/climateiq_merge_scenario_predictions_cf/main.py")
-    filename = "main.py"
-  }
-  # Add requirements.txt to the root of the zip file.
-  source {
-    content  = file("{path.module}/../../climateiq-frontend/cloud_functions/climateiq_merge_scenario_predictions_cf/requirements.txt")
-    filename = "requirements.txt"
-  }
-}
+  template {
+    task_count = 1
+    parallelism = 1
 
-resource "google_storage_bucket_object" "merge_scenario_predictions_source" {
-  name   = "frontend_merge_scenario_predictions_cloud_function_source.zip"
-  bucket = var.source_code_bucket.name
-  source = data.archive_file.merge_scenario_predictions_source.output_path
-}
-
-# Create a function triggered by writes to the spatialized chunk predictions bucket.
-resource "google_cloudfunctions2_function" "merge_scenario_predictions_function" {
-  depends_on = [
-    google_project_iam_member.gcs_pubsub_publishing,
-  ]
-
-  name        = "merge-scenario-predictions"
-  location    = lower(google_storage_bucket.spatialized_chunk_predictions.location) # The trigger must be in the same location as the bucket
-
-  build_config {
-    runtime     = "python311"
-    entry_point = "merge_scenario_predictions"
-    source {
-      storage_source {
-        bucket = var.source_code_bucket.name
-        object = google_storage_bucket_object.merge_scenario_predictions_source.name
+    template {
+      containers {
+        image = "${var.bucket_region}-docker.pkg.dev/${data.google_project.project.project_id}/cloud-run-containers/merge-scenario-predictions:latest"
+        env {
+          name  = "BUCKET_PREFIX"
+          value = var.bucket_prefix
+        }
+        resources {
+          limits = {
+            cpu    = "8"
+            memory = "32Gi"
+          }
+        }
       }
+      timeout = "86400s"
+      service_account = google_service_account.merge_scenario_predictions.email
+      max_retries = 0
     }
-  }
-
-  service_config {
-    available_memory      = "4Gi"
-    timeout_seconds       = 540
-    service_account_email = google_service_account.merge_scenario_predictions.email
-    environment_variables = {
-      BUCKET_PREFIX = var.bucket_prefix
-    }
-  }
-
-  event_trigger {
-    trigger_region        = lower(google_storage_bucket.spatialized_chunk_predictions.location) # The trigger must be in the same location as the bucket
-    event_type            = "google.cloud.storage.object.v1.finalized"
-    retry_policy          = "RETRY_POLICY_RETRY"
-    service_account_email = google_service_account.merge_scenario_predictions.email
-    event_filters {
-      attribute = "bucket"
-      value     = google_storage_bucket.spatialized_chunk_predictions.name
-    }
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      google_storage_bucket_object.merge_scenario_predictions_source
-    ]
   }
 }
--- modules/export_pipeline/trigger_export_pipeline.tf
+++ modules/export_pipeline/trigger_export_pipeline.tf
@@ -1,3 +1,9 @@
+# Create a service account used by the Job and Eventarc trigger
+resource "google_service_account" "trigger_export" {
+  account_id   = "gcf-trigger-export-sa"
+  display_name = "Used By Trigger Export Cloud Run Job"
+}
+
 # Create bucket for predictions input.
 resource "google_storage_bucket" "predictions" {
   name     = "${var.bucket_prefix}climateiq-predictions"
@@ -10,46 +16,109 @@ resource "google_storage_bucket" "chunk_predictions" {
   location = var.bucket_region
 }
 
-# Register a Pub/Sub topic for triggering subsequent steps in export pipeline.
-resource "google_pubsub_topic" "export_predictions_topic" {
-  name = "climateiq-spatialize-and-export-predictions"
-  message_retention_duration = "86600s"  # 24 hours
+# Grant permission to invoke workflows
+resource "google_project_iam_member" "trigger_export_workflows_invoker" {
+  project = data.google_project.project.id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.trigger_export.email}"
 }
 
-# Create a service account used by the function and Eventarc trigger
-resource "google_service_account" "trigger_export" {
-  account_id   = "gcf-trigger-export-sa"
-  display_name = "Used By Trigger Export Cloud Function"
+# Create a Cloud Workflow which watches the input bucket and triggers tasks.
+resource "google_workflows_workflow" "trigger_export_pipeline" {
+  name            = "trigger-export-pipeline"
+  region          = var.bucket_region
+  description     = "Watches climateiq-predictions bucket for new files and triggers trigger-export-pipeline CF"
+  service_account = google_service_account.trigger_export.id
+  source_contents = <<-EOF
+  main:
+      params: [event]
+      steps:
+          - init:
+              assign:
+                  - project_id: $${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")}
+                  - event_bucket: $${event.data.bucket}
+                  - event_file: $${event.data.name}
+                  - job_name: trigger-export-pipeline
+                  - job_location: ${var.bucket_region}
+          - run_job:
+              call: googleapis.run.v1.namespaces.jobs.run
+              args:
+                  name: $${"namespaces/" + project_id + "/jobs/" + job_name}
+                  location: $${job_location}
+                  body:
+                      overrides:
+                          containerOverrides:
+                              args: ["python3", "main.py", '$${event_file}']
+              result: job_execution
+          - finish:
+              return: $${job_execution}
+EOF
+
+  depends_on = [google_project_service.workflows]
 }
 
-# Grant permissions needed for the cloud function to run and receive event triggers.
-resource "google_project_iam_member" "trigger_export_invoking" {
-  project    = data.google_project.project.project_id
-  role       = "roles/run.invoker"
-  member     = "serviceAccount:${google_service_account.trigger_export.email}"
-  depends_on = [google_project_iam_member.gcs_pubsub_publishing]
-}
-
-resource "google_project_iam_member" "trigger_export_receiving" {
-  project    = data.google_project.project.project_id
+# Grant permission to receive events
+resource "google_project_iam_member" "trigger_export_event_receiver" {
+  project    = data.google_project.project.id
   role       = "roles/eventarc.eventReceiver"
   member     = "serviceAccount:${google_service_account.trigger_export.email}"
-  depends_on = [google_project_iam_member.trigger_export_invoking]
+  depends_on = [google_project_service.eventarc]
+}
+
+# Create an Eventarc trigger, routing Cloud Storage events to Workflows
+resource "google_eventarc_trigger" "prediction_file_created" {
+  name     = "prediction-file-created"
+  location = var.bucket_region
+
+  # Capture objects changed in the bucket
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.predictions.name
+  }
+
+  # Send events to Workflow
+  destination {
+    workflow = google_workflows_workflow.trigger_export_pipeline.id
+  }
+
+  service_account = google_service_account.trigger_export.id
+
+  depends_on = [google_project_service.eventarc]
+}
+
+# Grant permissions needed for the Cloud Run Job to run.
+resource "google_project_iam_member" "trigger_export_run_invoking" {
+  project    = data.google_project.project.project_id
+  role       = "roles/run.developer"
+  member     = "serviceAccount:${google_service_account.trigger_export.email}"
+}
+
+resource "google_project_iam_member" "trigger_export_run_viewing" {
+  project    = data.google_project.project.project_id
+  role       = "roles/run.viewer"
+  member     = "serviceAccount:${google_service_account.trigger_export.email}"
 }
 
 resource "google_project_iam_member" "trigger_export_artifactregistry_reader" {
   project    = data.google_project.project.project_id
   role       = "roles/artifactregistry.reader"
   member     = "serviceAccount:${google_service_account.trigger_export.email}"
-  depends_on = [google_project_iam_member.trigger_export_receiving]
 }
 
-# Grant permissions for the cloud function to publish messages to the Pub/Sub topic.
-resource "google_project_iam_member" "trigger_export_publishing" {
-  project = data.google_project.project.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.trigger_export.email}"
-  depends_on = [google_pubsub_topic.export_predictions_topic]
+resource "google_project_iam_member" "trigger_export_enqueuing" {
+  project    = data.google_project.project.project_id
+  role       = "roles/cloudtasks.enqueuer"
+  member     = "serviceAccount:${google_service_account.trigger_export.email}"
+}
+
+resource "google_service_account_iam_member" "trigger_export_run_as" {
+  role       = "roles/iam.serviceAccountUser"
+  member     = "serviceAccount:${google_service_account.trigger_export.email}"
+  service_account_id = google_service_account.spatialize_chunk_predictions.name
 }
 
 # Give read access to the predictions bucket.
@@ -59,79 +128,58 @@ resource "google_storage_bucket_iam_member" "trigger_export_predictions_reader" 
   member = "serviceAccount:${google_service_account.trigger_export.email}"
 }
 
-# Give write access to the chunk predictions bucket.
+# Give write access to the chunked predictions bucket.
 resource "google_storage_bucket_iam_member" "trigger_export_chunk_predictions_writer" {
   bucket = google_storage_bucket.chunk_predictions.name
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.trigger_export.email}"
 }
 
-# Place the source code for the cloud function into a GCS bucket.
-data "archive_file" "trigger_export_source" {
-  type        = "zip"
-  output_path = "${path.module}/files/trigger_export/cloud_function_source.zip"
+# Create Cloud Run Job.
+resource "google_cloud_run_v2_job" "trigger_export_pipeline" {
+  name         = "trigger-export-pipeline"
+  location     = var.bucket_region
+  project      = data.google_project.project.project_id
 
-  # Add main.py to the root of the zip file.
-  source {
-    content  = file("{path.module}/../../climateiq-frontend/cloud_functions/climateiq_trigger_export_pipeline_cf/main.py")
-    filename = "main.py"
-  }
-  # Add requirements.txt to the root of the zip file.
-  source {
-    content  = file("{path.module}/../../climateiq-frontend/cloud_functions/climateiq_trigger_export_pipeline_cf/requirements.txt")
-    filename = "requirements.txt"
-  }
-}
+  template {
+    task_count = 1
+    parallelism = 1
 
-resource "google_storage_bucket_object" "trigger_export_source" {
-  name   = "frontend_trigger_export_cloud_function_source.zip"
-  bucket = var.source_code_bucket.name
-  source = data.archive_file.trigger_export_source.output_path
-}
-
-# Create a function triggered by writes to the predictions bucket.
-resource "google_cloudfunctions2_function" "trigger_export_function" {
-  depends_on = [
-    google_project_iam_member.gcs_pubsub_publishing,
-  ]
-
-  name        = "trigger-export-pipeline"
-  location    = lower(google_storage_bucket.predictions.location) # The trigger must be in the same location as the bucket
-
-  build_config {
-    runtime     = "python311"
-    entry_point = "trigger_export_pipeline"
-    source {
-      storage_source {
-        bucket = var.source_code_bucket.name
-        object = google_storage_bucket_object.trigger_export_source.name
+    template {
+      containers {
+        image = "${var.bucket_region}-docker.pkg.dev/${data.google_project.project.project_id}/cloud-run-containers/trigger-export-pipeline:latest"
+        env {
+          name  = "BUCKET_PREFIX"
+          value = var.bucket_prefix
+        }
+        resources {
+          limits = {
+            cpu    = "1"
+            memory = "4Gi"
+          }
+        }
       }
+      timeout = "86400s"
+      service_account = google_service_account.trigger_export.email
+      max_retries = 0
     }
   }
+}
 
-  service_config {
-    available_memory      = "4Gi"
-    timeout_seconds       = 540
-    service_account_email = google_service_account.trigger_export.email
-    environment_variables = {
-      BUCKET_PREFIX = var.bucket_prefix
-    }
+# Create Task Queue
+resource "google_cloud_tasks_queue" "spatialize_chunk_predictions_queue" {
+  name = "spatialize-chunk-predictions-q"
+  location = var.bucket_region
+
+  rate_limits {
+    max_concurrent_dispatches = 25
+    max_dispatches_per_second = 1
   }
 
-  event_trigger {
-    trigger_region        = lower(google_storage_bucket.predictions.location) # The trigger must be in the same location as the bucket
-    event_type            = "google.cloud.storage.object.v1.finalized"
-    retry_policy          = "RETRY_POLICY_RETRY"
-    service_account_email = google_service_account.trigger_export.email
-    event_filters {
-      attribute = "bucket"
-      value     = google_storage_bucket.predictions.name
-    }
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      google_storage_bucket_object.trigger_export_source
-    ]
+  retry_config {
+    max_attempts = 9
+    max_backoff = "3600s"
+    min_backoff = "90s"
+    max_doublings = 16
   }
 }
